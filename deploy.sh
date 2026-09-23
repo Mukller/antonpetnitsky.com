0e863841e9e57dba68287fa2e48f601eff518ac7
Anton <anton.petnitsky@gmail.com>
2026-09-23 15:27:16 +0300

    Fix deploy: add privacy-policy.html and terms.html to copy list (fix 404)

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ if [ -d /tmp/site-deploy ]; then rm -rf /tmp/site-deploy; fi
 git clone https://github.com/Mukller/antonpetnitsky.com.git /tmp/site-deploy
 
 # Copy files
-for f in index.html cv-ru.html cv-en.html 404.html 50x.html sitemap.xml sitemap-pages.xml robots.txt ap-favicon.svg ap-favicon.png og.png current-server-ip.txt get-server-ip.sh; do
+for f in index.html cv-ru.html cv-en.html 404.html 50x.html sitemap.xml sitemap-pages.xml robots.txt privacy-policy.html terms.html ap-favicon.svg ap-favicon.png og.png current-server-ip.txt get-server-ip.sh; do
     if [ -f "/tmp/site-deploy/$f" ]; then
         sudo cp "/tmp/site-deploy/$f" "$SITE_DIR/"
     fi
